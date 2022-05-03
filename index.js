144b1b32bb1f64e056eeb3b7ab4fc7b598676f27
mahopon <tan.cy01@outlook.com>
2022-05-03 20:59:39 +0800

    Changed precision of lat and lat

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function processData(mrt, data) {
             continue;
 
         $(`#${mrtNoWs}data`).append(`<li id="${mrtNoWs}item${searchVAL.slice(searchVAL.length -1, searchVAL.length)}">`+searchVAL.slice(searchAfter, searchVAL.length)+"</li> ");
-        $(`#${mrtNoWs}item${searchVAL.slice(searchVAL.length -1, searchVAL.length)}`).append("<p>"+currentSelection.LATITUDE + ", "+ currentSelection.LONGITUDE+"</p>");
+        $(`#${mrtNoWs}item${searchVAL.slice(searchVAL.length -1, searchVAL.length)}`).append("<p>"+ parseFloat(currentSelection.LATITUDE).toFixed(7) + ", "+ parseFloat(currentSelection.LONGITUDE).toFixed(7)+"</p>");
     }
 
     if (data.totalNumPages > 1 && data.pageNum + 1 <= data.totalNumPages) {
